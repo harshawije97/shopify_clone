bcab56ffc9909b01826ebac086467a411327f1bb
harshawije97 <harshawije99@gmail.com>
2026-02-17 14:15:04 +0530

    signup page completed

--- lib/features/auth/views/pages/signup_page.dart
+++ lib/features/auth/views/pages/signup_page.dart
@@ -5,48 +5,80 @@ import 'package:shopify_clone/resources/themes/app_color_pallete.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
+  /*
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  */
 
   @override
   State<SignupPage> createState() => _SignupPageState();
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    // Form validation
+    formKey.currentState?.validate();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Sign Up",
-              style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 28),
-            TextFieldInput(placeholder: "Full Name"),
-            const SizedBox(height: 16),
-            TextFieldInput(placeholder: "Email"),
-            const SizedBox(height: 16),
-            TextFieldInput(placeholder: "Password"),
-            const SizedBox(height: 16),
-            AuthButton(),
-            const SizedBox(height: 20),
-            RichText(
-              text: TextSpan(
-                text: "Already have an account? ",
-                style: Theme.of(context).textTheme.titleMedium,
-                children: [
-                  TextSpan(
-                    text: "Sign In",
-                    style: TextStyle(color: Pallete.gradient1),
-                  ),
-                ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Sign Up",
+                style: TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
               ),
-            ),
-          ],
+              const SizedBox(height: 28),
+              TextFieldInput(
+                placeholder: "Full Name",
+                controller: nameController,
+              ),
+              const SizedBox(height: 16),
+              TextFieldInput(placeholder: "Email", controller: emailController),
+              const SizedBox(height: 16),
+              TextFieldInput(
+                placeholder: "Password",
+                controller: passwordController,
+                isPassword: true,
+              ),
+              const SizedBox(height: 16),
+              AuthButton(),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: "Already have an account? ",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: "Sign In",
+                      style: TextStyle(color: Pallete.gradient1),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/auth/views/widgets/text_field_input.dart
+++ lib/features/auth/views/widgets/text_field_input.dart
@@ -2,10 +2,30 @@ import 'package:flutter/material.dart';
 
 class TextFieldInput extends StatelessWidget {
   final String placeholder;
-  const TextFieldInput({super.key, required this.placeholder});
+  final TextEditingController controller;
+  final bool isPassword;
 
+  // Constructor
+  const TextFieldInput({
+    super.key,
+    required this.placeholder,
+    required this.controller,
+    this.isPassword = false,
+  });
+
+  // UI method
   @override
   Widget build(BuildContext context) {
-    return TextFormField(decoration: InputDecoration(hintText: placeholder));
+    return TextFormField(
+      decoration: InputDecoration(hintText: placeholder),
+      controller: controller,
+      validator: (value) {
+        if (value!.trim().isEmpty) {
+          return "Please enter $placeholder";
+        }
+        return null;
+      },
+      obscureText: isPassword,
+    );
   }
 }
